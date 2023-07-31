c903eb79c0e5808243b735fc7dc581087e905c27
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-01 00:36:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -160,3 +160,15 @@ function lightModeProperties() {
   console.log("setting dark mode to false");
 }
 
+profilecontainer.classList.toggle("active");
+searchbar.classList.toggle("active");
+getUserData(url + "thepranaygupta");
+
+
+//INITIALISE UI
+function init() {
+  //initialise dark-mode variable to false;
+  //darkMode = true -> dark mode enable karna h 
+  //darMode = false -> light mode enable karna h 
+  darkMode = false;
+  
